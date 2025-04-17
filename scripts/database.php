d342c6d6eb1e25a09fa11af4cec1277c38858f35
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-04-17 17:49:37 +0200

    added try catch to connection logic

--- scripts/database.php
+++ scripts/database.php
@@ -4,10 +4,15 @@ $dbname = 'ump_db';
 $username = 'root';
 $password = '';
 
-$connection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-]);
+try {
+        $connection = new PDO("mysql:host=$host;dbname=$dbname", $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ]);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+
 
 function fetch_user_by_id($connection, $id)
 {
