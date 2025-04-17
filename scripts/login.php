72c04b4736a834230cf15f52a84be4b81851d1fb
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-04-17 20:19:15 +0200

    registration is working without response to user

--- scripts/login.php
+++ scripts/login.php
@@ -7,8 +7,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-    $stmt = $connection->prepare("SELECT * FROM users WHERE email = :username AND password = :password");
-    $stmt->bindParam(':username', $username);
+    $stmt = $connection->prepare("SELECT * FROM users WHERE email = :email AND password = :password");
+    $stmt->bindParam(':email', $email);
     $stmt->bindParam(':password', $password);
     $stmt->execute();
 
@@ -19,18 +19,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if ($result && password_verify($password, $result['password'])) {
         // Password is correct, set session variables
         $_SESSION['logged'] = true;
-        $_SESSION['username'] = $result['email'];
+        $_SESSION['email'] = $result['email'];
         $_SESSION['user_id'] = $result['id'];
         header("Location: ../marketplace.php"); // Redirect to dashboard page
         exit(); // Ensure no further code is executed after the redirect
     } else {
         // Invalid credentials
-        echo "<script>alert('Invalid login credentials')</script>";
-        header("Location: ../login.php"); // Redirect to login page
-        exit(); // Ensure no further code is executed after the alert
+        header("Location: ../login.php?Invalid_credentials"); // Redirect to login page
+        exit(); // Ensure no further code is executed after the redirect
     }
 } else {
-    echo "<script>alert(Invalid login attempt.)</script>";
     header("Location: ../index.php"); // Redirect to dashboard page
     exit(); // Ensure no further code is executed if the request method is not POST
 }
