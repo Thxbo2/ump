7843c49047eb84e625bf34113048f78b130751fd
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-04-17 20:05:20 +0200

    added cross check function to see if user already exists

--- scripts/register.php
+++ scripts/register.php
@@ -20,10 +20,30 @@ function create_user($connection, $username, $email, $password)
         $statement = $connection->prepare("INSERT INTO users (username, email, password) VALUES (?,?,?)");
         $statement->execute([$username, $email, $hashed_password]);
         header("Location: ../login.php"); // Redirect to login page
+        exit(); // Ensure no further code is executed after the redirect
     } catch (PDOException $e) {
         // Log the error message (optional) and display a user-friendly message
         log_error($connection, $e->getMessage() . "Failed registration attempt", "Registration", $email);
         header("Location: ../register.php?error=Registration_failed. Please_try_again_later."); // Redirect to register page with error message
+        exit(); // Ensure no further code is executed after the redirect
     }
 }
+
+function crosscheck_user($connection, $email)
+{
+    $statement = $connection->prepare("SELECT * FROM users WHERE email=?");
+    $statement->execute([$email]);
+    $result = $statement->fetch();
+    return $result;
+    if ($result) {
+        // User already exists, handle accordingly (e.g., show error message)
+        header("Location: ../register.php?error=User_already_exists.");
+        exit(); // Ensure no further code is executed after the redirect
+    } else {
+        // If the user does not exist, proceed with registration
+        create_user($connection, $username, $email, $password);
+        exit(); // Ensure no further code is executed after the redirect
+    }
+}
+    
 ?>
