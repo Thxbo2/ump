5683311b3a4bd190c3ca456cadce8e7749f53303
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-04-17 17:55:23 +0200

    modifying login logic

--- scripts/login.class.php
+++ scripts/login.class.php
@@ -1,6 +1,36 @@
 <?php
 session_start();
 require_once 'database.php';
-require_once 'functions.php';
-require_once 'auth.php';
+
+// Handle login form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
+    $id;
+
+    $stmt = $conn->prepare("SELECT * FROM users WHERE email = :username AND password = :password");
+    $stmt->bindParam(':username', $username);
+    $stmt->bindParam(':password', $password);
+    $stmt->execute();
+
+    // Fetch the result
+    $result = $stmt->fetch(PDO::FETCH_ASSOC);
+    $customer_id = $result['id']; // Fetch the user_id from the query result
+
+    if ($stmt->rowCount() > 0) {
+        $_SESSION['logged'] = true;
+        $_SESSION['username'] = $username;
+        $_SESSION['user_id'] = $user_id;
+        header("Location: ../user_dashboard.php"); // Redirect to dashboard page
+        exit(); // Ensure no further code is executed after the redirect
+    } else {
+        echo "<script>alert('Invalid login credentials')</script>";
+        header("Location: ../login.php"); // Redirect to dashboard page
+        exit(); // Ensure no further code is executed after the alert
+    }
+} else {
+    echo "<script>alert(Invalid login attempt.)</script>";
+    header("Location: ../user_dashboard.php"); // Redirect to dashboard page
+    exit(); // Ensure no further code is executed if the request method is not POST
+}
 ?>
