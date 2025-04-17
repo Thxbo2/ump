b410170df98e2a1cc642e29dfbfde0b73aef79bf
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-04-17 17:22:19 +0200

    renamed previous files of the old UI, merging new UI

---
+++ login-old.php
@@ -0,0 +1,76 @@
+<?php
+include __DIR__.'/scripts/start.php';
+require_once __DIR__.'/scripts/functions.php';
+require_once __DIR__.'/scripts/database.php';
+require_once __DIR__.'/scripts/auth.php';
+require_once __DIR__.'/scripts/forms.php';
+
+if (session_has_user()) {
+    redirect('marketplace.php');
+}
+
+$form_error = null;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+    if (! validate_form_field_is_filled('email')) {
+        $form_error = "The username field is required";
+    }
+
+    if (! validate_form_field_is_filled('password')) {
+        $form_error = "The passsword field is required";
+    }
+
+    if ($form_error == null) {
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $user = fetch_user_by_email($connection, $email);
+
+        if (! $user) {
+            $form_error = "The provided details do not match any account";
+        }
+
+        if ($form_error == null && ! password_verify($password, $user['password'])) {
+            $form_error = "The provided details do not match any account";
+        }
+
+        if ($form_error == null) {
+            login_user($user['id']);
+
+            redirect('marketplace.php');
+        }
+    }
+}
+?>
+
+<?php $pageTitle = "Login | Universal Marketplace" ?>
+<?php include __DIR__.'/partials/page_start.php' ?>
+
+<?php include __DIR__.'/partials/guest_header.php' ?>
+
+<main class="main-content">
+    <h2 class="section-heading">Sign In</h2>
+
+    <form class="form" method="POST">
+        <?php if ($form_error) : ?>
+            <p class="form-field__error"><?= $form_error ?></p>
+        <?php endif; ?>
+
+        <div class="form__input-field">
+            <label for="email">Email</label>
+            <input type="email" name="email" id="email" placeholder="Enter Email" required><br>
+        </div>
+
+        <div class="form__input-field">
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" placeholder="Enter Password" required><br>
+        </div>
+
+        <button class="form__submit" type="submit" name="login">Login</button>
+    </form>
+</main>
+
+
+<?php include __DIR__.'/partials/page_end.php' ?>
+<?php include __DIR__.'/scripts/end.php' ?>
--- login.php
+++ login.php
@@ -1,76 +1,263 @@
-<?php
-include __DIR__.'/scripts/start.php';
-require_once __DIR__.'/scripts/functions.php';
-require_once __DIR__.'/scripts/database.php';
-require_once __DIR__.'/scripts/auth.php';
-require_once __DIR__.'/scripts/forms.php';
+<!DOCTYPE html>
+<html lang="en">
 
-if (session_has_user()) {
-    redirect('marketplace.php');
-}
+  <head>
 
-$form_error = null;
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500,700" rel="stylesheet">
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    <title>Pixie Template - Contact</title>
 
-    if (! validate_form_field_is_filled('email')) {
-        $form_error = "The username field is required";
-    }
+    <!-- Bootstrap core CSS -->
+    <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 
-    if (! validate_form_field_is_filled('password')) {
-        $form_error = "The passsword field is required";
-    }
 
-    if ($form_error == null) {
-        $email = $_POST['email'];
-        $password = $_POST['password'];
+    <!-- Additional CSS Files -->
+    <link rel="stylesheet" href="assets/css/fontawesome.css">
+    <link rel="stylesheet" href="assets/css/tooplate-main.css">
+    <link rel="stylesheet" href="assets/css/owl.css">
+    <link rel="stylesheet" href="assets/css/flex-slider.css">
+<!--
+Tooplate 2114 Pixie
+https://www.tooplate.com/view/2114-pixie
+-->
+  </head>
 
-        $user = fetch_user_by_email($connection, $email);
-
-        if (! $user) {
-            $form_error = "The provided details do not match any account";
-        }
-
-        if ($form_error == null && ! password_verify($password, $user['password'])) {
-            $form_error = "The provided details do not match any account";
-        }
-
-        if ($form_error == null) {
-            login_user($user['id']);
-
-            redirect('marketplace.php');
-        }
-    }
-}
-?>
-
-<?php $pageTitle = "Login | Universal Marketplace" ?>
-<?php include __DIR__.'/partials/page_start.php' ?>
-
-<?php include __DIR__.'/partials/guest_header.php' ?>
-
-<main class="main-content">
-    <h2 class="section-heading">Sign In</h2>
-
-    <form class="form" method="POST">
-        <?php if ($form_error) : ?>
-            <p class="form-field__error"><?= $form_error ?></p>
-        <?php endif; ?>
-
-        <div class="form__input-field">
-            <label for="email">Email</label>
-            <input type="email" name="email" id="email" placeholder="Enter Email" required><br>
+  <body>
+    
+    <!-- Pre Header -->
+    <div id="pre-header">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <span>Suspendisse laoreet magna vel diam lobortis imperdiet</span>
+          </div>
         </div>
+      </div>
+    </div>
 
-        <div class="form__input-field">
-            <label for="password">Password</label>
-            <input type="password" name="password" id="password" placeholder="Enter Password" required><br>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark static-top">
+      <div class="container">
+        <a class="navbar-brand" href="#"><img src="assets/images/header-logo.png" alt=""></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+          <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
+            <li class="nav-item active">
+              <a class="nav-link" href="login.php">Login</a>
+              <span class="sr-only">(current)</span>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="products.php">Products</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="about.php">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.php">Contact Us</a>
+            </li>
+          </ul>
         </div>
+      </div>
+    </nav>
 
-        <button class="form__submit" type="submit" name="login">Login</button>
-    </form>
-</main>
+    <!-- Page Content -->
+    <!-- About Page Starts Here -->
+    <div class="contact-page">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <div class="line-dec"></div>
+              <h1>Login</h1>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="right-content">
+              <div class="container">
+                <form id="contact" action="" method="post">
+                  <?php if ($form_error) : ?>
+                      <p class="form-field__error"><?= $form_error ?></p>
+                  <?php endif; ?>
+                  <div class="row">
+                    <div class="col-md-6">
+                      <fieldset>
+                        <input name="name" type="text" class="form-control" id="name" placeholder="Your name..." required="">
+                      </fieldset>
+                    </div>
+                    <div class="col-md-6">
+                      <fieldset>
+                        <input name="email" type="text" class="form-control" id="email" placeholder="Your email..." required="">
+                      </fieldset>
+                    </div>
+                    <div class="col-md-12">
+                      <fieldset>
+                        <input name="subject" type="text" class="form-control" id="subject" placeholder="Subject..." required="">
+                      </fieldset>
+                    </div>
+                    <div class="col-md-12">
+                      <fieldset>
+                        <textarea name="message" rows="6" class="form-control" id="message" placeholder="Your message..." required=""></textarea>
+                      </fieldset>
+                    </div>
+                    <div class="col-md-12">
+                      <fieldset>
+                        <button type="submit" id="form-submit" class="button">Send Message</button>
+                      </fieldset>
+                    </div>
+                    <div class="col-md-12">
+                      <div class="share">
+                        <h6>You can also keep in touch on: <span><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-linkedin"></i></a><a href="#"><i class="fa fa-twitter"></i></a></span></h6>
+                      </div>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div> <!-- col-md-6 ends here -->
+          <div class="col-md-6">
+            <div id="map">
+            		<!-- How to change your own map point
+                           1. Go to Google Maps
+                           2. Click on your location point
+                           3. Click "Share" and choose "Embed map" tab
+                           4. Copy only URL and paste it within the src="" field below
+                    -->
+
+              <img src="assets/images/login-bg.jpg" width="100%" height="500px" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- About Page Ends Here -->
+
+    <!-- Subscribe Form Starts Here -->
+    <div class="subscribe-form">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="section-heading">
+              <div class="line-dec"></div>
+              <h1>Subscribe on PIXIE now!</h1>
+            </div>
+          </div>
+          <div class="col-md-8 offset-md-2">
+            <div class="main-content">
+              <p>Godard four dollar toast prism, authentic heirloom raw denim messenger bag gochujang put a bird on it celiac readymade vice.</p>
+              <div class="container">
+                <form id="subscribe" action="" method="get">
+                  <div class="row">
+                    <div class="col-md-7">
+                      <fieldset>
+                        <input name="email" type="text" class="form-control" id="email" 
+                        onfocus="if(this.value == 'Your Email...') { this.value = ''; }" 
+                    	onBlur="if(this.value == '') { this.value = 'Your Email...';}"
+                    	value="Your Email..." required="">
+                      </fieldset>
+                    </div>
+                    <div class="col-md-5">
+                      <fieldset>
+                        <button type="submit" id="form-submit" class="button">Subscribe Now!</button>
+                      </fieldset>
+                    </div>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Subscribe Form Ends Here -->
 
 
-<?php include __DIR__.'/partials/page_end.php' ?>
-<?php include __DIR__.'/scripts/end.php' ?>
+    
+    <!-- Footer Starts Here -->
+    <div class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="logo">
+              <img src="assets/images/header-logo.png" alt="">
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="footer-menu">
+              <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Help</a></li>
+                <li><a href="#">Privacy Policy</a></li>
+                <li><a href="#">How It Works ?</a></li>
+                <li><a href="#">Contact Us</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="social-icons">
+              <ul>
+                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+                <li><a href="#"><i class="fa fa-rss"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Footer Ends Here -->
+
+
+    <!-- Sub Footer Starts Here -->
+    <div class="sub-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="copyright-text">
+              <p>Copyright &copy; 2019 Company Name 
+                
+                - Design: <a rel="nofollow" href="https://www.facebook.com/tooplate">Tooplate</a></p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Sub Footer Ends Here -->
+
+
+    <!-- Bootstrap core JavaScript -->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+
+    <!-- Additional Scripts -->
+    <script src="assets/js/custom.js"></script>
+    <script src="assets/js/owl.js"></script>
+    <script src="assets/js/isotope.js"></script>
+    <script src="assets/js/flex-slider.js"></script>
+
+
+    <script language = "text/Javascript"> 
+      cleared[0] = cleared[1] = cleared[2] = 0; //set a cleared flag for each field
+      function clearField(t){                   //declaring the array outside of the
+      if(! cleared[t.id]){                      // function makes it static and global
+          cleared[t.id] = 1;  // you could use true and false, but that's more typing
+          t.value='';         // with more chance of typos
+          t.style.color='#fff';
+          }
+      }
+    </script>
+
+
+  </body>
+
+</html>
