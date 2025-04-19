02f75fdf93e70859acdf74a3092f4028e865ac6a
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-04-19 16:01:46 +0200

    linked footer through reference file

---
+++ partials/footer.php
@@ -0,0 +1,50 @@
+<!-- Footer Starts Here -->
+<div class="footer">
+    <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+        <div class="logo">
+            <img src="assets/images/header-logo.png" alt="">
+        </div>
+        </div>
+        <div class="col-md-12">
+        <div class="footer-menu">
+            <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="help.php">Help</a></li>
+            <li><a href="policy.php">Privacy Policy</a></li>
+            <li><a href="contact.php">Contact Us</a></li>
+            </ul>
+        </div>
+        </div>
+        <div class="col-md-12">
+        <div class="social-icons">
+            <ul>
+            <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
+            <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+            </ul>
+        </div>
+        </div>
+    </div>
+    </div>
+</div>
+<!-- Footer Ends Here -->
+
+
+<!-- Sub Footer Starts Here -->
+<div class="sub-footer">
+    <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+        <div class="copyright-text">
+            <p>Copyright &copy; 2025 Universal Market Place 
+            
+            - Design: <a rel="nofollow" href="https://www.facebook.com/tooplate">Tooplate</a></p>
+        </div>
+        </div>
+    </div>
+    </div>
+</div>
+<!-- Sub Footer Ends Here -->
