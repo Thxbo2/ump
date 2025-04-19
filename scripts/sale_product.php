71be1f6cd6ae480ab9972aa669dc56ccdc01ea09
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-04-19 10:40:17 +0200

    removed block that shows error

--- scripts/sale_product.php
+++ scripts/sale_product.php
@@ -31,27 +31,6 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     header("Location: ../sale.php?error=Invalid_request_method.");
     exit();
 }
-    $name = $_POST['product_name'];
-    $price = $_POST['product_price'];
-    $image = $_FILES['product_image']['name'];
-    $description = $_POST['description'];
-
-    // Validate inputs
-    if (empty($name) || empty($price) || empty($image) || empty($description)) {
-        header("Location: ../sale.php?error=All_fields_are_required.");
-        exit(); // Ensure no further code is executed after the redirect
-    }
-
-    // Move uploaded image to a specific directory (e.g., 'uploads/')
-    $target_dir = "../uploads/";
-    $target_file = $target_dir . basename($_FILES["product_image"]["name"]);
-    move_uploaded_file($_FILES["product_image"]["tmp_name"], $target_file);
-
-    create_product($connection, $user_id, $name, $price, $image, $description);
-} else {
-    header("Location: ../sale.php?error=Invalid_request_method.");
-    exit(); // Ensure no further code is executed after the redirect
-}
 
 function create_product($connection, $user_id, $name, $price, $image, $description)
 {
