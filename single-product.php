2c1febd61be0505a79af20e240b2e584f88eb57d
Wanga Kanjala <75719614+V014@users.noreply.github.com>
2025-04-20 12:25:31 +0200

    enabled dynamic fetch of selected product

--- single-product.php
+++ single-product.php
@@ -109,35 +109,13 @@ https://www.tooplate.com/view/2114-pixie
             <div class="product-slider">
               <div id="slider" class="flexslider">
                 <ul class="slides">
-                  <li>
-                    <img src="assets/images/big-01.jpg" />
-                  </li>
-                  <li>
-                    <img src="assets/images/big-02.jpg" />
-                  </li>
-                  <li>
-                    <img src="assets/images/big-03.jpg" />
-                  </li>
-                  <li>
-                    <img src="assets/images/big-04.jpg" />
-                  </li>
+                <img src="images/products/<?php echo htmlspecialchars($product['image']); ?>" alt="<?php echo htmlspecialchars($product['name']); ?>" />
                   <!-- items mirrored twice, total of 12 -->
                 </ul>
               </div>
               <div id="carousel" class="flexslider">
                 <ul class="slides">
-                  <li>
-                    <img src="assets/images/thumb-01.jpg" />
-                  </li>
-                  <li>
-                    <img src="assets/images/thumb-02.jpg" />
-                  </li>
-                  <li>
-                    <img src="assets/images/thumb-03.jpg" />
-                  </li>
-                  <li>
-                    <img src="assets/images/thumb-04.jpg" />
-                  </li>
+                <img src="images/products/<?php echo htmlspecialchars($product['image']); ?>" alt="<?php echo htmlspecialchars($product['name']); ?>" />
                   <!-- items mirrored twice, total of 12 -->
                 </ul>
               </div>
@@ -145,25 +123,23 @@ https://www.tooplate.com/view/2114-pixie
           </div>
           <div class="col-md-6">
             <div class="right-content">
-              <h4>Single Product Name</h4>
-              <h6>$55.00</h6>
-              <p>Proin commodo, diam a ultricies sagittis, erat odio rhoncus metus, eu feugiat lorem lacus aliquet arcu. Curabitur in gravida nisi, non placerat nibh. Praesent sit amet diam ultrices, commodo turpis id, dignissim leo. Suspendisse mauris massa, porttitor non fermentum vel, ullamcorper scelerisque velit. </p>
-              <span>7 left on stock</span>
-              <form action="" method="get">
-                <label for="quantity">Quantity:</label>
-                <input name="quantity" type="quantity" class="quantity-text" id="quantity" 
-                	onfocus="if(this.value == '1') { this.value = ''; }" 
-                    onBlur="if(this.value == '') { this.value = '1';}"
-                    value="1">
-                <input type="submit" class="button" value="Order Now!">
+              <h4><?php echo htmlspecialchars($product['name']); ?></h4>
+              <h6>MWK<?php echo htmlspecialchars(number_format($product['price'], 2)); ?></h6>
+              <p><?php echo htmlspecialchars($product['description']); ?></p>
+              <span><?php echo htmlspecialchars($product['quantity']); ?> left in stock</span>
+              <form action="order.php" method="post">
+                  <label for="quantity">Quantity:</label>
+                  <input name="quantity" type="number" class="quantity-text" id="quantity" min="1" max="<?php echo htmlspecialchars($product['quantity']); ?>" value="1" required>
+                  <input type="hidden" name="product_id" value="<?php echo htmlspecialchars($product['id']); ?>">
+                  <input type="submit" class="button" value="Order Now!">
               </form>
               <div class="down-content">
-                <div class="categories">
-                  <h6>Category: <span><a href="#">Pants</a>,<a href="#">Women</a>,<a href="#">Lifestyle</a></span></h6>
-                </div>
-                <div class="share">
-                  <h6>Share: <span><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-linkedin"></i></a><a href="#"><i class="fa fa-twitter"></i></a></span></h6>
-                </div>
+                  <div class="categories">
+                      <h6>Category: <span><a href="#"><?php echo htmlspecialchars($product['category']); ?></a></span></h6>
+                  </div>
+                  <div class="share">
+                      <h6>Share: <span><a href="#"><i class="fa fa-facebook"></i></a><a href="#"><i class="fa fa-linkedin"></i></a><a href="#"><i class="fa fa-twitter"></i></a></span></h6>
+                  </div>
               </div>
             </div>
           </div>
